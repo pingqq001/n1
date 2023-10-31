56228208f0ea688699ac3f92b394b4ca33604da9
pingqq001 <105734691+pingqq001@users.noreply.github.com>
2023-10-31 17:22:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,6 @@ echo 'src-git istore https://github.com/luochongjun/istore.git' >>feeds.conf.def
 echo 'src-git openclash https://github.com/vernesong/openclash.git' >>feeds.conf.default
 
 
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
